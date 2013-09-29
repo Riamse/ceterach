0812036d427e1308a368ad26ccbd60f502474ac9
riamse2 <riamse@protonmail.com>
2013-09-28 18:04:25 -0700

    require arrow, to be implemented later

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ from ceterach import __version__
 def read_long_description():
     return open("README.md").read()
 
-required_packages = ['requests>=1.0.3']
+required_packages = ['requests>=1.0.3', 'arrow>=0.3.5']
 
 setup(name='ceterach',
       version=__version__,
