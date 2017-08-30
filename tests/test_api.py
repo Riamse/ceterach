2a52e5dc2503ec5f04529e39d8d3b240c8379b51
Geoffrey Mon <geofbot@gmail.com>
2017-08-30 11:30:13 -0400

    Fully embrace the pytest way of life

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,8 +1,8 @@
-from unittest import TestCase
-
+import pytest
 import requests_mock
+from decorator import decorator as dec
 
-from ceterach import api
+import ceterach as c
 
 # Mock MediaWiki API URL
 WIKI_BASE = 'mock://a.wiki/w/api.php'
@@ -25,66 +25,73 @@ def _test_for(responses, num_called=1):
         responses[i] = {'json': r}
 
     def decorator(inner):
-        def decorator_inner(*args):
+        def decorator_inner(f, *args, **kwargs):
             with requests_mock.mock() as rqm:
                 rqm.register_uri(requests_mock.ANY, WIKI_BASE, responses)
-                inner(*args)
+                f(*args, **kwargs)
             assert rqm.call_count == num_called
 
-        return decorator_inner
+        return dec(decorator_inner, inner)
 
     return decorator
 
 
-class TestMediaWiki(TestCase):
-    def setup_class(self):
-        self.api = api.MediaWiki(WIKI_BASE)
+@pytest.fixture
+def api():
+    return c.api.MediaWiki(WIKI_BASE)
 
-    def test_repr(self):
-        assert repr(self.api).startswith('MediaWiki(api_url=\'' + WIKI_BASE)
 
-    def test_eq(self):
-        assert self.api == api.MediaWiki(WIKI_BASE)
-        assert self.api != api.MediaWiki(reversed(WIKI_BASE))
+def test_repr(api):
+    assert repr(api).startswith('MediaWiki(api_url=\'' + WIKI_BASE)
 
-    def test_page(self):
-        page = self.api.page('Noodling')
-        assert page.title == 'Noodling'
 
-    @_test_for({'tokens': {'csrftoken': TEST_TOKEN}})
-    def test_tokens_action_tokens(self):
-        self.api.set_token()
-        assert ('csrf', TEST_TOKEN) in self.api.tokens.items()
+def test_eq(api):
+    assert api == c.api.MediaWiki(WIKI_BASE)
+    assert api != c.api.MediaWiki(reversed(WIKI_BASE))
 
-    @_test_for([
-        {'error': {
-            'code': 'unknown_action',
-            'info': 'Unrecognized value for parameter \'action\': '
-                    'tokens'
-        }},
-        q({'pages': {
-            '-1': {
-                'edittoken': TEST_TOKEN
-            }
-        }})
-    ], 2)
-    def test_tokens_action_query(self):
-        self.api.set_token()
-        assert ('edit', TEST_TOKEN) in self.api.tokens.items()
 
-    @_test_for(q({'namespaces': [{'id': 0,
-                                  'case': 'first-letter',
-                                  'content': '',
-                                  '*': ''},
-                                 {'id': 1337,
-                                  'case': 'first-letter',
-                                  'canonical': 'Spam',
-                                  '*': 'Spam'}]}))
-    def test_namespaces(self):
-        ns = self.api.namespaces
-        assert len(ns) == 2
-        assert ns[1337] == 'Spam'
+def test_page(api):
+    page = api.page('Noodling')
+    assert page.title == 'Noodling'
 
-    @_test_for({})
-    def test_logout(self):
-        assert self.api.logout()
+
+@_test_for({'tokens': {'csrftoken': TEST_TOKEN}})
+def test_tokens_action_tokens(api):
+    api.set_token()
+    assert ('csrf', TEST_TOKEN) in api.tokens.items()
+
+
+@_test_for([
+    {'error': {
+        'code': 'unknown_action',
+        'info': 'Unrecognized value for parameter \'action\': '
+                'tokens'
+    }},
+    q({'pages': {
+        '-1': {
+            'edittoken': TEST_TOKEN
+        }
+    }})
+], 2)
+def test_tokens_action_query(api):
+    api.set_token()
+    assert ('edit', TEST_TOKEN) in api.tokens.items()
+
+
+@_test_for(q({'namespaces': [{'id': 0,
+                              'case': 'first-letter',
+                              'content': '',
+                              '*': ''},
+                             {'id': 1337,
+                              'case': 'first-letter',
+                              'canonical': 'Spam',
+                              '*': 'Spam'}]}))
+def test_namespaces(api):
+    ns = api.namespaces
+    assert len(ns) == 2
+    assert ns[1337] == 'Spam'
+
+
+@_test_for({})
+def test_logout(api):
+    assert api.logout()
